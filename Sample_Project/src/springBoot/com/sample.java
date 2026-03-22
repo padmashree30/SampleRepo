f575b2e1aea0853fa9534cd4e34ec448807ad9d1
Lenovo <Lenovo@PadmashreeShridhar>
2026-03-22 10:36:28 +0530

    New Final Updated Changes Added...

--- Sample_Project/src/springBoot/com/sample.java
+++ Sample_Project/src/springBoot/com/sample.java
@@ -1,15 +1,18 @@
 package springBoot.com;
 
+import java.util.Scanner;
+
 public class sample {
 
-public static void main(String[] args) {
-	
-	for(int i = 0; i<=10; i++) {
-		
-		System.out.println("i"+i);
+	public static void main(String[] args) {
+		Scanner sa = new Scanner(System.in);
+		System.out.println("Enter You Name:");
+		String name = sa.next();
+		System.out.println("Name is:" + name);
+		for (int i = 0; i <= 10; i++) {
+
+			System.out.println("i" + i);
+		}
+		System.out.println("This is new change");
 	}
-	System.out.println("This is new change");
-	
-	
-}
 }
